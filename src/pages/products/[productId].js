42114c5afc338605ad95ba8b478a681af72e66f8
Praise Dare <praisedare27@gmail.com>
2023-05-14 19:41:54 +0100

    adding tailwind

--- src/pages/products/[productId].js
+++ src/pages/products/[productId].js
@@ -1,13 +1,27 @@
 export default function ProductDetailsPage({ product, params }) {
 	return <>
-		<h1>Product {product.id} details</h1>
-		<pre>{ JSON.stringify(params) }</pre>
+		<h1>{product.name} <i>[{product.id}]</i></h1>
+        <div class="flex space-between">
+            <i>{product.category}</i>
+            <span>${product.price}</span>
+        </div>
+        <p>{product.description}</p>
+		<pre>params: { JSON.stringify(params) }</pre>
 	</>
 }
 
-export function getServerSideProps(context) {
+export async function getProduct(id) {
+    return await (
+        await fetch(`http://localhost:3399/products/${id}`)
+    )
+    .json()
+}
+
+export async function getServerSideProps(context) {
 	const {params} = context
-	const product = {id: params.productId};
+	const id = params.productId
+
+    const product = await getProduct(id)
 
 	return {
 		props: {
